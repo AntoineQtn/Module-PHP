53b52cb39950d9b55dd8764adc1dc47ab0bf8690
Antoine QUINTIN <antoine.quintin@le-campus-numerique.fr>
2025-05-20 12:26:59 +0200

    IT4 - 3.2 : Création de catalog-with-keys.php avec des tableaux associatifs et affichage de ces éléments ordonné en balises html

---
+++ catalog-with-keys.php
@@ -0,0 +1,32 @@
+<?php
+$iphone = [
+    "name" => "iPhone",
+    "price" => 45000,
+    "weight" => 200,
+    "discount" => 10,
+    "picture_url" => "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/IPhone_16_Pro_Vector.svg/800px-IPhone_16_Pro_Vector.svg.png",
+];
+//echo var_dump($iphone) . "<br>";
+echo "<div>";
+echo "<h3>nom du produit : $iphone[name]</h3>";
+echo "<p>Prix : $iphone[price]</p>";
+echo "<img src=$iphone[picture_url] alt='Image produit' />";
+echo "</div>";
+
+/*$iPad = [
+    "name" => "iPad",
+    "price" => 79900,
+    "weight" => 500,
+    "discount" => null,
+    "picture_url" => "https://media.materiel.net/r250/products/MN0005046452_1.jpg",
+];*/
+//echo var_dump($iPad) . "<br>";
+
+/*$iMac = [
+    "name" => "iMac",
+    "price" => 450000,
+    "weight" => 5000,
+    "discount" => 50,
+    "picture_url" => "https://static.fnac-static.com/multimedia/Images/FR/MDM/19/b2/a1/10596889/1540-1/tsp20250110194849/iMac-Apple-27-Ecran-Retina-5K-Intel-Core-i5-8-Go-RAM-1-To-Fusion-Drive-Argent-MRQY2FN-2019.jpg",
+];*/
+//echo var_dump($iMac) . "<br>";
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include('header.php'); ?>
 <?php include('simple-catalog.php'); ?>
+<?php include('catalog-with-keys.php');?>
 <?php include('item.php'); ?>
 <?php include('footer.php'); ?>
